0ac3d0fbc9fbdda711a191959ab7768bcf80fe0d
Massawe14 <ramadhanimassawe14@gmail.com>
2022-08-29 11:05:44 +0300

    Update

--- app/src/main/java/org/learnprograming/screenmirroring/ThirdFragment.java
+++ app/src/main/java/org/learnprograming/screenmirroring/ThirdFragment.java
@@ -1,13 +1,17 @@
 package org.learnprograming.screenmirroring;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
-public class ThirdFragment extends Fragment {
+public class ThirdFragment extends Fragment implements View.OnClickListener {
+
+    Button client;
 
     public static ThirdFragment newInstance(){
         return new ThirdFragment();
@@ -21,6 +25,17 @@ public class ThirdFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_third, container, false);
+        View view = inflater.inflate(R.layout.fragment_third, container, false);
+
+        client = view.findViewById(R.id.client);
+        client.setOnClickListener(this);
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i = new Intent(getActivity(), MainClint.class);
+        startActivity(i);
     }
 }
