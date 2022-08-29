5dc2a73d93a58e788400fbbd3fcd6f59eb1956b8
Massawe14 <ramadhanimassawe14@gmail.com>
2022-08-29 12:00:27 +0300

    Update

--- app/src/main/java/org/learnprograming/screenmirroring/SecondFragment.java
+++ app/src/main/java/org/learnprograming/screenmirroring/SecondFragment.java
@@ -1,17 +1,13 @@
 package org.learnprograming.screenmirroring;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
-public class SecondFragment extends Fragment implements View.OnClickListener {
-    
-    Button server;
+public class SecondFragment extends Fragment {
 
     public static SecondFragment newInstance(){
         return new SecondFragment();
@@ -25,17 +21,6 @@ public class SecondFragment extends Fragment implements View.OnClickListener {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_third, container, false);
-
-        server = view.findViewById(R.id.server);
-        server.setOnClickListener(this);
-
-        return view;
-    }
-
-    @Override
-    public void onClick(View v) {
-        Intent i = new Intent(getActivity(), MainServer.class);
-        startActivity(i);
+        return inflater.inflate(R.layout.fragment_second, container, false);
     }
 }
--- app/src/main/java/org/learnprograming/screenmirroring/ThirdFragment.java
+++ app/src/main/java/org/learnprograming/screenmirroring/ThirdFragment.java
@@ -1,17 +1,13 @@
 package org.learnprograming.screenmirroring;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
-public class ThirdFragment extends Fragment implements View.OnClickListener {
-
-    Button client;
+public class ThirdFragment extends Fragment {
 
     public static ThirdFragment newInstance(){
         return new ThirdFragment();
@@ -25,17 +21,6 @@ public class ThirdFragment extends Fragment implements View.OnClickListener {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_third, container, false);
-
-        client = view.findViewById(R.id.client);
-        client.setOnClickListener(this);
-
-        return view;
-    }
-
-    @Override
-    public void onClick(View v) {
-        Intent i = new Intent(getActivity(), MainClint.class);
-        startActivity(i);
+        return inflater.inflate(R.layout.fragment_third, container, false);
     }
 }
